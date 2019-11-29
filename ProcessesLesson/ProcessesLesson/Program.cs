cf363e64130eceaf96e3cf3ab027ce9ee5c21f13
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-11-29 20:54:58 +0600

    Lesson commit

---
+++ ProcessesLesson/ProcessesLesson/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProcessesLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Process.Start("https://mail.ru");
+            //Console.WriteLine("Введите 1 для запуска калькулятора");
+            //var currentProcess = Process.GetCurrentProcess();
+            //Console.WriteLine($"{currentProcess.Id} - {currentProcess.ProcessName}");
+
+            //currentProcess.Kill();
+
+            var chromeProcess = Process.GetProcessesByName("devenv");
+            foreach(var chrome in chromeProcess)
+            {
+                chrome.Kill();
+            }
+            //if(int.TryParse(Console.ReadLine(), out var number) && number == 1)
+            //{
+                
+            //Process.Start("calc");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ПОШЕЛ НАХУЙ");
+            //    Console.ReadKey();
+            //    Console.WriteLine("Я сказал: ПОШЕЛ НАХУЙ");
+            //}
+        }
+    }
+}
